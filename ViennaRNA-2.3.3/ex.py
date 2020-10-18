99deb7673440cd8c8549e91c6b54d340b573c613
cacotsuki <rodrigo.sp9@hotmail.com>
2020-10-17 21:03:33 -0300

    First Commit

---
+++ ViennaRNA-2.3.3/ex.py
@@ -0,0 +1,5 @@
+from subprocess import Popen, PIPE
+p = Popen(['./src/bin/RNAplot'], stdin=PIPE, shell=True)
+p = Popen(['CCCCCCCCAAAAGGGGGGGG'], stdin=PIPE, shell=True)
+p = Popen(['.((((((((....))))))))'], stdin=PIPE, shell=True)
+p = Popen(['@'], stdin=PIPE, shell=True)
